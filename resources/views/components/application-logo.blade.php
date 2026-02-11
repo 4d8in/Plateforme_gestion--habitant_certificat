38aaa59d94427b33b7ba842e7a0b06104962e536
Edwin Tchouateu <edwintchouateu54@gmail.com>
2026-02-11 23:54:06 +0000

    changement de docker

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,37 +1,42 @@
-<svg viewBox="0 0 140 140" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
+<svg xmlns="http://www.w3.org/2000/svg"
+     viewBox="0 0 128 128"
+     aria-label="Logo"
+     {{ $attributes->merge(['class' => '']) }}>
     <g fill="currentColor">
-        <!-- Gear -->
-        <g transform="translate(70 90)">
-            <circle r="22" />
-            <rect x="-5" y="-46" width="10" height="12" rx="2" />
-            <rect x="-5" y="34" width="10" height="12" rx="2" />
-            <rect x="34" y="-5" width="10" height="12" rx="2" transform="rotate(90 39 1)" />
-            <rect x="-44" y="-5" width="10" height="12" rx="2" transform="rotate(90 -39 1)" />
-            <rect x="26" y="-34" width="10" height="12" rx="2" transform="rotate(45 31 -28)" />
-            <rect x="-36" y="26" width="10" height="12" rx="2" transform="rotate(45 -31 32)" />
-            <rect x="26" y="26" width="10" height="12" rx="2" transform="rotate(135 31 32)" />
-            <rect x="-36" y="-34" width="10" height="12" rx="2" transform="rotate(135 -31 -28)" />
+        <!-- Gear base -->
+        <g transform="translate(64 82)">
+            <circle r="26"/>
+            <rect x="-5" y="-52" width="10" height="12" rx="2"/>
+            <rect x="-5" y="40" width="10" height="12" rx="2"/>
+            <rect x="40" y="-5" width="10" height="12" rx="2" transform="rotate(90 45 1)"/>
+            <rect x="-50" y="-5" width="10" height="12" rx="2" transform="rotate(90 -45 1)"/>
+            <rect x="28" y="-38" width="10" height="12" rx="2" transform="rotate(45 33 -32)"/>
+            <rect x="-38" y="28" width="10" height="12" rx="2" transform="rotate(45 -33 34)"/>
+            <rect x="28" y="28" width="10" height="12" rx="2" transform="rotate(135 33 34)"/>
+            <rect x="-38" y="-38" width="10" height="12" rx="2" transform="rotate(135 -33 -32)"/>
         </g>
 
         <!-- Ground plate -->
-        <rect x="38" y="82" width="64" height="6" rx="3" />
+        <rect x="34" y="70" width="60" height="8" rx="3"/>
 
-        <!-- Building back tower -->
-        <rect x="44" y="38" width="14" height="44" rx="2" />
-        <rect x="47" y="44" width="8" height="8" rx="1" fill="#fff" />
-        <rect x="47" y="58" width="8" height="8" rx="1" fill="#fff" />
-        <rect x="47" y="72" width="8" height="8" rx="1" fill="#fff" />
+        <!-- Back tower -->
+        <rect x="42" y="30" width="16" height="44" rx="2"/>
+        <rect x="45" y="36" width="10" height="10" rx="1" fill="#ffffff"/>
+        <rect x="45" y="50" width="10" height="10" rx="1" fill="#ffffff"/>
+        <rect x="45" y="64" width="10" height="10" rx="1" fill="#ffffff"/>
 
         <!-- Main building -->
-        <rect x="60" y="26" width="26" height="56" rx="3" />
-        <rect x="66" y="32" width="6" height="8" rx="1" fill="#fff" />
-        <rect x="78" y="32" width="6" height="8" rx="1" fill="#fff" />
-        <rect x="66" y="46" width="6" height="8" rx="1" fill="#fff" />
-        <rect x="78" y="46" width="6" height="8" rx="1" fill="#fff" />
-        <rect x="66" y="60" width="6" height="8" rx="1" fill="#fff" />
-        <rect x="78" y="60" width="6" height="8" rx="1" fill="#fff" />
+        <rect x="60" y="18" width="28" height="56" rx="3"/>
+        <g fill="#ffffff">
+            <rect x="66" y="24" width="8" height="10" rx="1"/>
+            <rect x="78" y="24" width="8" height="10" rx="1"/>
+            <rect x="66" y="38" width="8" height="10" rx="1"/>
+            <rect x="78" y="38" width="8" height="10" rx="1"/>
+            <rect x="66" y="52" width="8" height="10" rx="1"/>
+            <rect x="78" y="52" width="8" height="10" rx="1"/>
+        </g>
 
         <!-- Porch -->
-        <rect x="64" y="78" width="18" height="6" rx="1" />
+        <rect x="66" y="70" width="16" height="8" rx="1"/>
     </g>
 </svg>
